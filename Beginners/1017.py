f0cdc580e20b0f51e0452164bb85342229dec2f3
angelo-francisco <158621356+angelo-francisco@users.noreply.github.com>
2024-04-08 21:00:54 +0100

    1017 solved

---
+++ Beginners/1017.py
@@ -0,0 +1,7 @@
+tempo_gasto = float(input(''))
+velocidade = float(input(''))
+
+distancia_percorrida = velocidade*tempo_gasto
+litros_necessarios = distancia_percorrida / 12
+
+print(f'{litros_necessarios:.3f}')
